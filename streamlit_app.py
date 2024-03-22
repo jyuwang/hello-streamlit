40245cb633d3b1e3367327a98eaa116f823642af
Joey <100096661+jyuwang@users.noreply.github.com>
2024-03-22 13:42:26 -0700

    Update streamlit_app.py with app specific code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+import openai
+import time
+# from openai import OpenAI
 from datetime import datetime, timedelta
 
 # Page title
@@ -65,93 +68,62 @@ df = df.sort_values(by=['Status', 'ID'], ascending=[False, False])
 if 'df' not in st.session_state:
     st.session_state.df = df
 
-# Sort dataframe
-def sort_df():
-    st.session_state.df = edited_df.copy().sort_values(by=['Status', 'ID'], ascending=[False, False])
+def refactorCode(input_html, input_css, input_js):
+    
+    
+    output_html = "testhtml"
+    output_css = "testcss"
+    output_js = "testjs"
+    
+    return output_html, output_css, output_js
+    
+    
+    
+
+
 
 
 # Tabs for app layout
-tabs = st.tabs(['Write a ticket', 'Ticket Status and Analytics'])
+tabs = st.tabs(['Submit Code', 'Submit DB'])
 
 recent_ticket_number = int(max(st.session_state.df.ID).split('-')[1])
 
 with tabs[0]:
-  with st.form('addition'):
-    issue = st.text_area('Description of issue')
-    priority = st.selectbox('Priority', ['High', 'Medium', 'Low'])
+  with st.form('Code'):
+    html = st.text_area('HTML Code')
+    css = st.text_area('CSS Code')
+    js = st.text_area('JS Code')
+    # priority = st.selectbox('Priority', ['High', 'Medium', 'Low'])
+    submit = st.form_submit_button('Submit')
+
+  if submit:
+      today_date = datetime.now().strftime('%m-%d-%Y')
+      outputs = refactorCode(html, css, js)
+      df2 = pd.DataFrame([{'ID': f'TICKET-{recent_ticket_number+1}',
+                           'HTML': outputs[0],
+                           'CSS': outputs[1],
+                            'JS': outputs[2],
+                           'Date Submitted': today_date
+                          }])
+      st.write('Request submitted!')
+      time.sleep(5)
+      st.dataframe(df2, use_container_width=True, hide_index=True)
+      st.session_state.df = pd.concat([st.session_state.df, df2], axis=0).sort_values(by=['Status', 'ID'], ascending=[False, False])
+
+with tabs[1]:
+  with st.form('DB'):
+    originalDB = st.text_area('DB')
+    # priority = st.selectbox('Priority', ['High', 'Medium', 'Low'])
     submit = st.form_submit_button('Submit')
 
   if submit:
       today_date = datetime.now().strftime('%m-%d-%Y')
       df2 = pd.DataFrame([{'ID': f'TICKET-{recent_ticket_number+1}',
-                           'Issue': issue,
-                           'Status': 'Open',
-                           'Priority': priority,
+                           'Original DB': originalDB,
                            'Date Submitted': today_date
                           }])
-      st.write('Ticket submitted!')
+      st.write('Request submitted!')
       st.dataframe(df2, use_container_width=True, hide_index=True)
       st.session_state.df = pd.concat([st.session_state.df, df2], axis=0).sort_values(by=['Status', 'ID'], ascending=[False, False])
-
-with tabs[1]:
-  status_col = st.columns((3,1))
-  with status_col[0]:
-      st.subheader('Support Ticket Status')
-  with status_col[1]:
-      st.write(f'No. of tickets: `{len(st.session_state.df)}`')
-
-  st.markdown('**Things to try:**')
-  st.info('1️⃣ Update Ticket **Status** or **Priority** and see how plots are updated in real-time!')
-  st.success('2️⃣ Change values in **Status** column from *"Open"* to either *"In Progress"* or *"Closed"*, then click on the **Sort DataFrame by the Status column** button to see the refreshed DataFrame with the sorted **Status** column.')
-
-  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212,
-                column_config={'Status': st.column_config.SelectboxColumn(
-                                            'Status',
-                                            help='Ticket status',
-                                            options=[
-                                                'Open',
-                                                'In Progress',
-                                                'Closed'
-                                            ],
-                                            required=True,
-                                            ),
-                               'Priority': st.column_config.SelectboxColumn(
-                                           'Priority',
-                                            help='Priority',
-                                            options=[
-                                                'High',
-                                                'Medium',
-                                                'Low'
-                                            ],
-                                            required=True,
-                                            ),
-                             })
-  st.button('🔄 Sort DataFrame by the Status column', on_click=sort_df)
-  
-  # Status plot
-  st.subheader('Support Ticket Analytics')
-  col = st.columns((1,3,1))
-    
-  with col[0]:
-      n_tickets_queue = len(st.session_state.df[st.session_state.df.Status=='Open'])
-      
-      st.metric(label='First response time (hr)', value=5.2, delta=-1.5)
-      st.metric(label='No. of tickets in the queue', value=n_tickets_queue, delta='')
-      st.metric(label='Avg. ticket resolution time (hr)', value=16, delta='')
       
       
-  with col[1]:
-      status_plot = alt.Chart(edited_df).mark_bar().encode(
-          x='month(Date Submitted):O',
-          y='count():Q',
-          xOffset='Status:N',
-          color = 'Status:N'
-      ).properties(title='Ticket status in the past 6 months', height=300).configure_legend(orient='bottom', titleFontSize=14, labelFontSize=14, titlePadding=5)
-      st.altair_chart(status_plot, use_container_width=True, theme='streamlit')
-      
-  with col[2]:
-      priority_plot = alt.Chart(edited_df).mark_arc().encode(
-                          theta="count():Q",
-                          color="Priority:N"
-                      ).properties(title='Current ticket priority', height=300).configure_legend(orient='bottom', titleFontSize=14, labelFontSize=14, titlePadding=5)
-      st.altair_chart(priority_plot, use_container_width=True, theme='streamlit')
